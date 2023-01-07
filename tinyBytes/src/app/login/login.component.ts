6abd1a6279b7a49dda4ede78f51f7b6e459d6224
CDenille <carringtondenille@gmail.com>
2023-01-07 20:31:38 +0000

    updating login route

--- tinyBytes/src/app/login/login.component.ts
+++ tinyBytes/src/app/login/login.component.ts
@@ -53,7 +53,7 @@ export class LoginComponent {
 
   loginService(options: any): void{
     const requestObservable: Observable<any> = this.http.post<any>(
-      'https://tinybytes.herokuapp.com/logIn', 
+      'https://tinybytes-production.up.railway.app/logIn', 
       { 
         "email": this.email, 
         "password": this.password

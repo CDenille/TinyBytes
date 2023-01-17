8bc35db835d482353fddf864b90f379e1f280432
CDenille <carringtondenille@gmail.com>
2023-01-17 17:19:21 +0000

    singup redirect

--- tinyBytes/src/app/signup/signup.component.ts
+++ tinyBytes/src/app/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { EnrollmentService } from '../service/enrollment.service';
+// import { EnrollmentService } from '../service/enrollment.service';
 import { UntypedFormBuilder, Validators } from '@angular/forms';
 import { User } from '../user';
 import { Router } from '@angular/router';
@@ -9,25 +9,25 @@ import { Router } from '@angular/router';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent{
-  signupForm = this.formBuilder.group({
-    firstName:['', [Validators.required, Validators.pattern('[a-zA-Z]+')]],
-    lastName: ['', [Validators.required, Validators.pattern('[a-zA-Z]+')]],
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(6)]]
-  })
+//   signupForm = this.formBuilder.group({
+//     firstName:['', [Validators.required, Validators.pattern('[a-zA-Z]+')]],
+//     lastName: ['', [Validators.required, Validators.pattern('[a-zA-Z]+')]],
+//     email: ['', [Validators.required, Validators.email]],
+//     password: ['', [Validators.required, Validators.minLength(6)]]
+//   })
 
-newUser = new User;
-  constructor(
-    private enrollmentService:EnrollmentService,
-    private formBuilder: UntypedFormBuilder,
-    private router: Router
-    ){}
+// newUser = new User;
+//   constructor(
+//     private enrollmentService:EnrollmentService,
+//     private formBuilder: UntypedFormBuilder,
+//     private router: Router
+//     ){}
 
  
-  onSubmit(){
-  console.log('Form value',this.signupForm)
-    this.enrollmentService.enroll(this.signupForm.value)
-  .subscribe(data =>{console.log(data)});
-    this.router.navigate(['http://tinybytes-production.up.railway.app'])
-  }
+//   onSubmit(){
+//   console.log('Form value',this.signupForm)
+//     this.enrollmentService.enroll(this.signupForm.value)
+//   .subscribe(data =>{console.log(data)});
+//     this.router.navigate(['http://localhost:4200'])
+//   }
 }

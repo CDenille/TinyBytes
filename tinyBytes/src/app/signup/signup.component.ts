f03ee871888805388659e7fb1022487496a85d8b
CDenille <carringtondenille@gmail.com>
2023-07-14 14:08:28 -0400

    implement user interface

--- tinyBytes/src/app/signup/signup.component.ts
+++ tinyBytes/src/app/signup/signup.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { EnrollmentService } from '../service/enrollment.service';
 import { UntypedFormBuilder, Validators } from '@angular/forms';
-import { User } from '../user';
+import { IUser } from '../interface/user';
 import { Router } from '@angular/router';
 
 @Component({
@@ -15,7 +15,7 @@ export class SignupComponent {
     private formBuilder: UntypedFormBuilder,
     private router: Router) { }
 
-  newUser = new User;
+  newUser!: IUser;
 
   signupForm = this.formBuilder.group({
     firstName: ['', [Validators.required, Validators.pattern('[a-zA-Z]+')]],
@@ -25,11 +25,14 @@ export class SignupComponent {
   })
 
   submit() {
-    console.log('Form value', this.signupForm)
+    console.log('Form value', this.signupForm);
     this.enrollmentService.enrollUser(this.signupForm.value)
-      .subscribe(data => { console.log(data) });
+      .subscribe({
+        next: user => { 
+          this.newUser = user,
+          console.log('Heres the new user', this.newUser)
+        }
+      });
     this.router.navigate(['http://localhost:4200'])
   }
-
-
 }

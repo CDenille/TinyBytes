9e457a906b04e7a65c1a551b4e0e4611461c5b51
CDenille <carringtondenille@gmail.com>
2023-07-14 13:38:51 -0400

    enrollment

--- tinyBytes/src/app/service/enrollment.service.ts
+++ tinyBytes/src/app/service/enrollment.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
+import { Observable, throwError} from 'rxjs';
+import { tap, catchError } from "rxjs/operators";
 import { User } from "../user";
 import {LoginComponent} from '../login/login.component'
 
@@ -11,9 +13,22 @@ export class EnrollmentService{
     constructor(private _http: HttpClient,
     private login:LoginComponent) { }
 
-    enrollUser(user:User){
+    enrollUser(user:User): Observable<any>{
         const headers = { 'content-type': 'application/json'}
         const body = JSON.stringify(user)
-        return this._http.post<any>(this.url, body, { 'headers': headers })
+        return this._http.post<any>(this.url, body, { 'headers': headers }).pipe(
+            catchError(this.handleError)
+        )
+    }
+
+    private handleError(err:HttpErrorResponse){
+        let errorMessage='';
+        if( err.error instanceof ErrorEvent){
+            errorMessage = `an error occured: ${err.error.message}`
+        }else{
+            errorMessage = `server returned code: ${err.status}, error message is: ${err.message}`
+        }
+        console.error(errorMessage);
+        return throwError(errorMessage)
     }
 }
